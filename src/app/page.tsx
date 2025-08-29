caab3a293a183b2d7ddfc2e21665972212afe2bb
Rohit Manvar <153635435+RohitManvar@users.noreply.github.com>
2025-08-29 21:08:02 +0530

    Add files via upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,7 +67,7 @@ export default function Page() {
             <BlurFade delay={BLUR_FADE_DELAY * 11}>
               <p className="prose max-w-full text-pretty font-sans text-sm text-muted-foreground dark:prose-invert">
                 I was born and raised in Junagadh. I&apos;ve always been curious about how things work, and early on, I loved exploring the mysteries of life and developing a sense of spiritual awareness.-<br />
-                <span className="bg-amber-100 dark:bg-amber-600 px-1 rounded">
+                <span className="bg-yellow-100 dark:bg-blue-900 dark:text-amber-100 px-1 rounded">
                   "I'm exerting myself to escape the same mind that traps me."
                 </span>
               </p>
