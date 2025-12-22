4f3b44061fc93e9c849a2fb20651528dbc0d6078
RohitManvar <Rohitmanvar123@gmail.com>
2025-12-22 20:02:29 +0530

    updation commit

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -15,8 +15,8 @@ export const DATA = {
 
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
-    // { href: "/blog", icon: NotebookIcon, label: "Blog" },
-    // { href: "/showcase", icon: SparklesIcon, label: "Showcase" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+    { href: "/showcase", icon: SparklesIcon, label: "Showcase" },
   ],
   contact: {
     email: "rohitmanvar.dev@gmail.com",
